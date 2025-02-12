e9006cdb1d413649c87ff4ac02b5254d6f576702
Farah Taounza El Abdellaoui <taounzafarah@gmail.com>
2025-02-12 23:42:39 +0100

    Route change

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatImageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [CatImageController::class, 'index']);
 
