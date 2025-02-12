e984b56d37abdc91bf5621b38540e1c93eea3ef5
Farah Taounza El Abdellaoui <taounzafarah@gmail.com>
2025-02-12 23:47:07 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatImageController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/', [CatImageController::class, 'index']);
 
